aaf32330dc797196d9364b161bd392736c8bef27
rixhieloomis <104064451+rixhieloomis@users.noreply.github.com>
2024-04-25 19:04:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
-resource "null_resource" "list_files" {
-  provisioner "local-exec" {
-    command = "ls -lahR"
+provisioner "remote-exec" {
+  inline = [
+    "powershell.exe -ExecutionPolicy Bypass -File script.ps1",
+  ]
+
+  connection {
+    type     = "winrm"
+    user     = "Administrator"
+    password = "${var.admin_password}"
+    host     = "${var.host}"
   }
 }
