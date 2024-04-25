39cb1affeacc4ae3a45407c20848240c29a06e9f
rixhieloomis <104064451+rixhieloomis@users.noreply.github.com>
2024-04-25 19:18:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,6 +7,11 @@ variable "host" {
 }
 
 variable "script_path" {
-  description = "The path to the PowerShell script that you want to execute."
-  default     = "path/to/script.ps1"
+  description = "The path to the PowerShell script that you want to execute on the target machine."
+  default     = "C:/App/say_hi.ps1"  # Path where the script will be placed on the target machine
+}
+
+variable "local_script_path" {
+  description = "The local path to the PowerShell script on your machine."
+  default     = "path/to/say_hi.ps1"  # Local path to your say_hi.ps1 script
 }
