e2afb1722b374a6b719398784128b26954588a4a
rixhieloomis <104064451+rixhieloomis@users.noreply.github.com>
2024-04-25 19:19:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,30 @@
-provisioner "remote-exec" {
-  inline = [
-    "powershell.exe -ExecutionPolicy Bypass -File ${var.script_path}",
-  ]
+provider "winrm" {
+  # Provider configuration if needed
+}
 
-  connection {
-    type     = "winrm"
-    user     = "Administrator"
-    password = "${var.admin_password}"
-    host     = "${var.host}"
+resource "null_resource" "transfer_and_execute_script" {
+  provisioner "file" {
+    source      = var.local_script_path
+    destination = var.script_path
+
+    connection {
+      type     = "winrm"
+      user     = "Administrator"
+      password = var.admin_password
+      host     = var.host
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "powershell.exe -ExecutionPolicy Bypass -File ${var.script_path}",
+    ]
+
+    connection {
+      type     = "winrm"
+      user     = "Administrator"
+      password = var.admin_password
+      host     = var.host
+    }
   }
 }
