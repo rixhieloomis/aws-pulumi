19b12eb671656e4318278be4eea77e107fa02098
rixhieloomis <104064451+rixhieloomis@users.noreply.github.com>
2024-07-22 18:36:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,35 +1,34 @@
 provider "local" {}
 
-resource "local_file" "user_info_script" {
-  content = <<-EOF
+# Create a script file
+resource "local_file" "script" {
+  content  = <<-EOT
     #!/bin/bash
-
+    
     # Get the current user
-    current_user=\$(whoami)
-
+    current_user=$(whoami)
+    
     # Get the current user's home directory
-    home_directory=\$(eval echo ~\$current_user)
-
+    home_directory=$(eval echo ~$current_user)
+    
     # Get the current user's group memberships
-    user_groups=\$(groups \$current_user)
-
+    user_groups=$(groups "$current_user")
+    
     # Get the permissions of the home directory
-    home_permissions=\$(ls -ld \$home_directory | awk '{print \$1}')
-
+    home_permissions=$(ls -ld "$home_directory" | awk '{print $1}')
+    
     # Print the information
-    echo "Current User: \$current_user"
-    echo "Home Directory: \$home_directory"
-    echo "Home Directory Permissions: \$home_permissions"
-    echo "Groups: \$user_groups"
-  EOF
-
-  filename = "${path.module}/user_info.sh"
+    echo "Current User: $current_user"
+    echo "Home Directory: $home_directory"
+    echo "Home Directory Permissions: $home_permissions"
+    echo "Groups: $user_groups"
+  EOT
+  filename = "${path.module}/info.sh"
 }
 
-resource "null_resource" "execute_user_info_script" {
-  depends_on = [local_file.user_info_script]
-
+# Use the script file with local-exec provisioner
+resource "null_resource" "run_script" {
   provisioner "local-exec" {
-    command = "bash ${local_file.user_info_script.filename}"
+    command = "bash ${local_file.script.filename}"
   }
 }
