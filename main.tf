5eac967a8014a82aec2420483ec5f62b3affdbe0
rixhieloomis <104064451+rixhieloomis@users.noreply.github.com>
2024-08-07 15:01:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-module "custom_git" {
-  source = "git@github.com:rixhieloomis/aws-terraform.git"
- }
+# module "custom_git" {
+#  source = "git@github.com:rixhieloomis/aws-terraform.git"
+#  }
 resource "null_resource" "hello_script" {
   # This resource serves as a placeholder and will not perform any actions other than executing the local-exec provisioner
   provisioner "local-exec" {
