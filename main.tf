9f1f2811d75222bb215f469e754427fd18e7b8e2
rixhieloomis <richard.loomis@stackguardian.io>
2024-08-12 19:57:42 +0530

    Invalid case 8

--- main.tf
+++ main.tf
@@ -2,12 +2,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "non-existent-bucket" # Incorrect S3 bucket name
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
 }
 
-output "non_existent_output" {
-  value = aws_instance.non_existent.id # Invalid reference
-}
 
