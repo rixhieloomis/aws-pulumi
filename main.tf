49070646c8e9f485dfe5ff4705c1423a99474e5b
rixhieloomis <richard.loomis@stackguardian.io>
2024-08-12 19:51:15 +0530

    Invalid case 2

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "example-invalid!" { # Invalid name due to "!"
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-# Missing closing brace for the resource block
+}
