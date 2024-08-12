dce9a162b98bf6e06180c93141551a576edac072
rixhieloomis <richard.loomis@stackguardian.io>
2024-08-12 19:58:07 +0530

    Invalid case 9

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "non-existent-bucket" # Incorrect S3 bucket name
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
-resource "aws_instance" "example" {
+resource "aws_instance" "example_a" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  depends_on    = [aws_instance.example_b] # Circular dependency
 }
+
+resource "aws_instance" "example_b" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  depends_on    = [aws_instance.example_a] # Circular dependency
+}
+
 
 
