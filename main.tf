02af0a8b9e8f52abacd25c7ba848d00037e462d1
rixhieloomis <104064451+rixhieloomis@users.noreply.github.com>
2024-08-19 10:11:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,19 @@
 # module "custom_git" {
 #  source = "git@github.com:rixhieloomis/aws-terraform.git"
 #  }
+variable "resource_count" {
+  default = 500
+}
+
 resource "null_resource" "hello_script" {
-  # This resource serves as a placeholder and will not perform any actions other than executing the local-exec provisioner
+  count = var.resource_count
+
   provisioner "local-exec" {
     command = "echo 'Hello, World!'"
   }
 }
-output "message_length" {
-  value = length("Hello, World")
+
+output "message_lengths" {
+  value = [for i in range(var.resource_count): length("Hello, World")]
 }
+
