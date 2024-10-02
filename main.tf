bd85705a57eb5a8065a1f52d35665d4f90bd9f23
rixhieloomis <104064451+rixhieloomis@users.noreply.github.com>
2024-10-02 16:16:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,13 @@
-# module "custom_git" {
-#  source = "git@github.com:rixhieloomis/aws-terraform.git"
-#  }
-variable "resource_count" {
-  default = 1
+provider "aws" {
+  region = "us-west-2"
 }
 
-variable "wait_time" {
-  default = 10
-}
-
-resource "null_resource" "hello_script" {
-  count = var.resource_count
-
-  triggers = {
-    timestamp = timestamp()
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-      echo 'Hello, World!'
-      sleep ${var.wait_time}
-    EOT
+data "aws_instances" "example" {
+  instance_tags = {
+    Environment = "production"
   }
 }
 
-output "message_lengths" {
-  value = [for i in range(var.resource_count): length("Hello, World!")]
+output "instance_ids" {
+  value = data.aws_instances.example.ids
 }
-
