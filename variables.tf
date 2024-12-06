23a4d9004bd73490e5562bb7790c1ca50882efbe
rixhieloomis <104064451+rixhieloomis@users.noreply.github.com>
2024-12-06 15:30:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,9 +8,3 @@ variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
 }
-
-variable "acl" {
-  description = "The access control list (ACL) of the S3 bucket"
-  type        = string
-  default     = "private"
-}
