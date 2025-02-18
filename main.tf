cffb8d11cbde8415fc91e2f1292fd3747e310ec4
rixhieloomis <104064451+rixhieloomis@users.noreply.github.com>
2025-02-18 10:40:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "http" {
-    address  = "https://api.app.stackguardian.io/api/v1/orgs/wicked-hop/wfgrps/Managed-backend/wfs/Managed-backend/artifacts/tfstate.json"
-    username = "richard.loomis@stackguardian.io"
-    password = "sgu_26w33hsaXkXgiFXX3Bnss"
-  }
-}
+# terraform {
+#   backend "http" {
+#     address  = "https://api.app.stackguardian.io/api/v1/orgs/wicked-hop/wfgrps/Managed-backend/wfs/Managed-backend/artifacts/tfstate.json"
+#     username = "richard.loomis@stackguardian.io"
+#     password = "sgu_26w33hsaXkXgiFXX3Bnss"
+#   }
+# }
 
 resource "null_resource" "workflow_example" {
   provisioner "local-exec" {
